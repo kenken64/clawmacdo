9aab4f12fa3b079a6f84c4cd07c69fd7fc0aff43
kenken64 <bunnyppl@hotmail.com>
2026-03-05 15:54:54 +0000

    fix: open ports 80/443 in cloud-init UFW rules
    HTTP and HTTPS ports were missing from cloud-init firewall setup.
    Had to be manually added via SSH during Tencent deploy debugging.

--- src/cloud_init.rs
+++ src/cloud_init.rs
@@ -30,6 +30,8 @@ runcmd:
   - ufw default deny incoming
   - ufw default allow outgoing
   - ufw allow 22/tcp
+  - ufw allow 80/tcp
+  - ufw allow 443/tcp
   - ufw allow 18789/tcp
   - ufw --force enable
 
